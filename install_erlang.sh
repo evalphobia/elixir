1963ba7c71be887852cd7aec7c33b7860f0a14f5
evalphobia <evalphobia@gmail.com>
2017-04-24 19:55:43 +0900

    Fix CI yml

---
+++ install_erlang.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+# credit: https://github.com/codeship/scripts/blob/master/languages/erlang.sh
+
+ERLANG_VERSION=${ERLANG_VERSION:="18.3"}
+ERLANG_PATH=${ERLANG_PATH:=$HOME/erlang}
+CACHED_DOWNLOAD="${HOME}/cache/erlang-OTP-${ERLANG_VERSION}.tar.gz"
+
+# credit: https://unix.stackexchange.com/questions/101080/realpath-command-not-found
+realpath() {
+    f=$@;
+    if [ -d "$f" ]; then
+        base="";
+        dir="$f";
+    else
+        base="/$(basename "$f")";
+        dir=$(dirname "$f");
+    fi;
+    dir=$(cd "$dir" && /bin/pwd);
+    echo "$dir$base"
+}
+
+mkdir -p "${ERLANG_PATH}"
+ERLANG_PATH=$(realpath "${ERLANG_PATH}")
+
+wget --continue --output-document "${CACHED_DOWNLOAD}" "https://s3.amazonaws.com/heroku-buildpack-elixir/erlang/cedar-14/OTP-${ERLANG_VERSION}.tar.gz"
+tar -xaf "${CACHED_DOWNLOAD}" --strip-components=1 --directory "${ERLANG_PATH}"
+"${ERLANG_PATH}/Install" -minimal "${ERLANG_PATH}"
+
+export PATH="${ERLANG_PATH}/bin:${PATH}"
+erl -eval '{ok, Version} = file:read_file(filename:join([code:root_dir(), "releases", erlang:system_info(otp_release), "OTP_VERSION"])), erlang:display(erlang:binary_to_list(Version)), halt().' -noshell | grep "${ERLANG_VERSION}"
